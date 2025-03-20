e5415115266fc95411832060b578d3c89c77fca2
Helga <helga128@gmail.com>
2025-03-20 23:39:49 +0000

    tengja við v4 framenda

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,15 @@ export const app = new Hono();
 app.use(prettyJSON());
 app.notFound((c) => c.json({ message: 'not found' }, 404));
 app.use('/*', cors({
-  origin: ['http://localhost:3000', 'https://vef2-v3-thb0.onrender.com'],
+  origin: ['http://localhost:3000', 'https://vef2-v4-gjvc.onrender.com'],
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }))
 app.route('/', api);
+app.options('*', (c) => {
+  return c.newResponse('', { status: 204 });
+});
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const HOST = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
